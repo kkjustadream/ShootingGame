b72fb6c15f6d121eb3f307e40f9b8052bf5db899
kkjustadream <kevinhsh0@gmail.com>
2023-05-31 16:35:56 -0700

    add moving target

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 backgrounds = []
 banners = []
 guns = []
-level = 1      # menu is at level 0
+target_images = [[], [], []]
+# target numbers for each level
+targets = {1: [10, 5, 3],
+           2: [12, 8, 5],
+           3: [15, 12, 8, 3]}
+level = 1  # menu is at level 0
 
 # put each level image into list
 for i in range(1, 4):
@@ -20,6 +25,16 @@ for i in range(1, 4):
     banners.append(pygame.image.load(f"assets/banners/{i}.png"))
     # scale gun from 500 to 100
     guns.append(pygame.transform.scale(pygame.image.load(f"assets/guns/{i}.png"), (100, 100)))
+    # level 1, 2 have 3 size of targets, level 3 has 4
+    if i < 3:
+        for j in range(1, 4):
+            target_images[i - 1].append(pygame.transform.scale(
+                pygame.image.load(f'assets/targets/{i}/{j}.png'), (120 - (j * 18), 80 - (j * 12))))
+    else:
+        for j in range(1, 5):
+            target_images[i - 1].append(pygame.transform.scale(
+                pygame.image.load(f'assets/targets/{i}/{j}.png'), (120 - (j * 18), 80 - (j * 12))))
+
 
 def draw_gun():
     """
@@ -59,6 +74,53 @@ def draw_gun():
                 pygame.draw.circle(screen, lasers[level - 1], (mouse_x, mouse_y), 5)
 
 
+def draw_level(coords):
+    if level == 1 or level == 2:
+        target_rects = [[], [], []]
+    else:
+        target_rects = [[], [], [], []]
+    # i size of target， 1,2 is 3 size, 3 is 4 size
+    for i in range(len(coords)):
+        # loop over all targets in same size
+        for j in range(len(coords[i])):
+            target_rects[i].append(pygame.rect.Rect((coords[i][j][0] + 20, coords[i][j][1]),
+                                                    (60 - i * 12, 60 - i * 12)))
+            screen.blit(target_images[level - 1][i], coords[i][j])
+    return target_rects
+
+def move_level(coords):
+    if level == 1 or level == 2:
+        max_val = 3
+    else:
+        max_val = 4
+    for i in range(max_val):
+        for j in range(len(coords[i])):
+            cur_coord = coords[i][j]
+            # biggest size out of screen
+            if cur_coord[0] < -150:
+                coords[i][j] = (WIDTH, cur_coord[1])
+            else:
+                coords[i][j] = (cur_coord[0] - 2 ** i, cur_coord[1])
+    return coords
+
+
+
+# initialize enemy coordinates, according to the number of targets
+one_coords = [[], [], []]
+two_coords = [[], [], []]
+three_coords = [[], [], [], []]
+for i in range(3):
+    my_list = targets[1]
+    for j in range(my_list[i]):
+        one_coords[i].append((WIDTH // (my_list[i]) * j, 300 - (i * 150) + 30 * (j % 2)))
+for i in range(3):
+    my_list = targets[2]
+    for j in range(my_list[i]):
+        two_coords[i].append((WIDTH // (my_list[i]) * j, 300 - (i * 150) + 30 * (j % 2)))
+for i in range(4):
+    my_list = targets[3]
+    for j in range(my_list[i]):
+        three_coords[i].append((WIDTH // (my_list[i]) * j, 300 - (i * 100) + 30 * (j % 2)))
 
 run = True
 while run:
@@ -68,6 +130,17 @@ while run:
     screen.fill("black")
     screen.blit(backgrounds[level - 1], (0, 0))
     screen.blit(banners[level - 1], (0, HEIGHT - 200))
+
+    if level == 1:
+        target_boxes = draw_level(one_coords)
+        one_coords = move_level(one_coords)
+    elif level == 2:
+        target_boxes = draw_level(two_coords)
+        two_coords = move_level(two_coords)
+    elif level == 3:
+        target_boxes = draw_level(three_coords)
+        three_coords = move_level(three_coords)
+
     if level > 0:
         draw_gun()
 
